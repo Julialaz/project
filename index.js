07268c9000459c9e587e4934bf9b9f57f1f22a10
Julia Lazurkevych <lazurkevich@yahoo.com>
2016-10-14 21:50:01 +0000

    add new file

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 /*global $*/
 /*global google*/
 
-	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
- e.preventDefault();
- 
-var target = this.hash,
- $target = $(target);
- 
-$('html, body').stop().animate({
- 'scrollTop': $target.offset().top
- }, 2000, 'swing', function () {
- window.location.hash = target;
- });
- });
+$('a[href^="#"]').bind('click.smoothscroll', function(e) {
+    e.preventDefault();
 
-$(function () {
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 2000, 'swing', function() {
+        window.location.hash = target;
+    });
+});
+
+$(function() {
     $('#send').click(function(e) {
         e.preventDefault();
         $.ajax({
-            url: "https://formspree.io/lazurkevich@yahoo.com", 
+            url: "https://formspree.io/lazurkevich@yahoo.com",
             method: "POST",
             data: {
-            	firstname: $('#firstname').val(),
-            	secondname: $('#secondname').val(),
+                firstname: $('#firstname').val(),
+                secondname: $('#secondname').val(),
                 email: $('#email').val(),
                 message: $('#message').val()
             },
@@ -31,28 +31,32 @@ $(function () {
             $('form').html('<h4><center>Thank you for reaching out! </br> Your message has been successfully sent. </br> I will contact you very soon!</center></h4>')
         }).fail(function(xhr, err) {
             $('form').html(xhr.statusText);
-        });        
+        });
     })
 });
 
- function initMap() {
-        var uluru = {lat: 49.85, lng: 24.0166666667};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-      
-var num = 730; 
+function initMap() {
+    var uluru = {
+        lat: 49.85,
+        lng: 24.0166666667
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
 
-$(window).bind('scroll', function () {
+var num = 730;
+
+$(window).bind('scroll', function() {
     if ($(window).scrollTop() > num) {
         $('.menu').addClass('fixed');
-    } else {
+    }
+    else {
         $('.menu').removeClass('fixed');
     }
 });
